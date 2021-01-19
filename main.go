02d25a362fe74ff594320dedb970ed5afb6fad57
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-01-19 23:55:57 +0900

    feat: add gorm

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"cheap-roulette-api/domain"
 	"net/http"
+
+	"gorm.io/driver/postgres"
+
+	"gorm.io/gorm"
 
 	"github.com/labstack/echo/v4"
 )
@@ -10,7 +15,15 @@ type Ping struct {
 	Status int `json:"status"`
 }
 
+var (
+	db  *gorm.DB
+	err error
+	dsn = "host=localhost user=roulette dbname=roulette password=roulette sslmode=disable"
+)
+
 func main() {
+	dbInit()
+
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
@@ -20,4 +33,12 @@ func main() {
 	})
 
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func dbInit() {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(domain.User{})
 }
