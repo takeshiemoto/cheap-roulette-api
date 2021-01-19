33cc7b94e9e5e2f399e46d954ab6b437fc9d26a6
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-01-19 23:40:46 +0900

    feat: added echo server

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Ping struct {
+	Status int `json:"status"`
+}
 
 func main() {
-	fmt.Println("Hello world")
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		s := new(Ping)
+		s.Status = http.StatusOK
+		return c.JSON(http.StatusOK, s)
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
 }
