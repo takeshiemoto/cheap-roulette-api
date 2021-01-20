6ac229728958c4d0c4d7691da55276ae988fe467
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-01-20 09:52:37 +0900

    feat: add controller

--- main.go
+++ main.go
@@ -4,16 +4,14 @@ import (
 	"cheap-roulette-api/domain"
 	"net/http"
 
+	"github.com/labstack/echo/v4/middleware"
+
 	"gorm.io/driver/postgres"
 
 	"gorm.io/gorm"
 
 	"github.com/labstack/echo/v4"
 )
-
-type Ping struct {
-	Status int `json:"status"`
-}
 
 var (
 	db  *gorm.DB
@@ -22,23 +20,54 @@ var (
 )
 
 func main() {
-	dbInit()
 
-	e := echo.New()
-
-	e.GET("/", func(c echo.Context) error {
-		s := new(Ping)
-		s.Status = http.StatusOK
-		return c.JSON(http.StatusOK, s)
-	})
-
-	e.Logger.Fatal(e.Start(":1323"))
-}
-
-func dbInit() {
+	// Database
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
-	db.AutoMigrate(domain.User{})
+	db.AutoMigrate(&domain.User{})
+
+	// Server
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+
+	// Routing
+	e.GET("/users", allUsers(db))
+	e.POST("/users", newUser(db))
+	e.DELETE("/users/:id", deleteUser(db))
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func allUsers(db *gorm.DB) func(ctx echo.Context) error {
+	return func(ctx echo.Context) error {
+		var users []domain.User
+		db.Find(&users)
+		return ctx.JSON(http.StatusOK, users)
+	}
+}
+
+func newUser(db *gorm.DB) func(ctx echo.Context) error {
+	return func(ctx echo.Context) error {
+		u := new(domain.User)
+		if err := ctx.Bind(u); err != nil {
+			return err
+		}
+		db.Create(&domain.User{
+			Name: u.Name,
+		})
+		return ctx.String(http.StatusOK, u.Name+" user successfully created")
+	}
+}
+
+func deleteUser(db *gorm.DB) func(ctx echo.Context) error {
+	return func(ctx echo.Context) error {
+		id := ctx.Param("id")
+		var user domain.User
+		db.Delete(&user, id)
+		return ctx.String(http.StatusOK, "user successfully deleted")
+	}
 }
